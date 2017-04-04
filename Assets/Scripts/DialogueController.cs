0c1707c86f1f1066b8eedb7821e3ae79f1b48169
JimiJokela1 <jimi.jokela@metropolia.fi>
2017-04-04 10:23:34 +0300

    Basic tile based map generator
    puts down tiles and click to move. leaves behind trail.

---
+++ Assets/Scripts/DialogueController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueController : MonoBehaviour {
+
+	GameObject Character;
+
+
+	void Start () {
+		
+	}
+
+
+	void Update () {
+		
+	}
+}
