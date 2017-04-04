36ab244d2957734384f1c702e2768e8862788b77
PyryKa <kanervapyry@gmail.com>
2017-04-04 12:04:45 +0300

    Eventgenerator template
    Ei valmis

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+	public Text textField;
+	public Image imageField;
+	private string name;
+
+	public void GenerateNPC(string name, Sprite sprite, Text text) {
+		this.name = name;
+		this.imageField.sprite = sprite;
+		this.textField.text = "SADSAD";
+
+	}
+
+
+}
