a50a8160242f006ed72ab9b8ab0f1a083e1482b9
PyryKa <kanervapyry@gmail.com>
2017-04-05 11:15:30 +0300

    Event valmis

---
+++ Assets/Event/Scripts/AssetScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetScript : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col) {
+		Debug.Log ("osu johonki");
+		if (col.gameObject.tag == "asset") {
+			Debug.Log ("osu");
+		}
+	}
+}
