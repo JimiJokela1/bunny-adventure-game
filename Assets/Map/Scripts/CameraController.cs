d487292b77f3bbcbad0bf1dd552f7f77a0968485
JimiJokela1 <jimi.jokela@metropolia.fi>
2017-04-07 13:38:56 +0300

    Event triggering continued
    camera zooms to a flag on the map.
    other stuff too.

---
+++ Assets/Map/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	private Transform target;
+	public float smoothing = 5f; // Camera follow speed
+	string mode;
+
+	Vector3 followOffset;
+	Vector3 eventZoomOffset;
+	Vector3 targetCamPos;
+
+	void Start () {
+		mode = "Follow";
+		target = GameObject.FindGameObjectWithTag ("Player").transform;
+
+		followOffset = transform.position - target.position; // Calculate initial offset
+		eventZoomOffset = followOffset / 10;
+	}
+
+	void FixedUpdate () {
+		if (mode == "Follow") {
+			targetCamPos = target.position + followOffset;
+		}
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.fixedDeltaTime);
+	} 
+
+	public void EventZoom(){
+		mode = "EventZoom";
+		targetCamPos = target.position + eventZoomOffset;
+	}
+}
