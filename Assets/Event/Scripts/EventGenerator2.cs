cc8befb6a16f0c283ecf3dfefc1449bd54a1baca
JimiJokela1 <jimi.jokela@metropolia.fi>
2017-04-25 09:37:33 +0300

    Revert "Merge branch 'master' into mapgenerator"
    This reverts commit 5d9777c1fd8b36e1ea141852518f8aae0d47762e.

--- Assets/Event/Scripts/EventGenerator2.cs
+++ Assets/Event/Scripts/EventGenerator2.cs
@@ -5,6 +5,11 @@ using UnityEngine.UI;
 
 public class EventGenerator2 : MonoBehaviour{
 
+
+
+	public NPC npc;
+	public LayerMask layerMask;
+
 	public List<GameObject> objectList = new List<GameObject>();
 	public List<GameObject> tempList = new List<GameObject> ();
 
@@ -17,9 +22,12 @@ public class EventGenerator2 : MonoBehaviour{
 	public int numberOfAssets = 10;
 	public int rayRange = 10;
 
+	public GameObject item;
+	public List<GameObject> assetList = new List<GameObject> ();
 
 	// Use this for initialization
 	void Start () {
+		npc = GameObject.FindObjectOfType (typeof(NPC)) as NPC;
 		terrainPosition.Add (new Vector3(0, 0, 0));
 		terrainPosition.Add (new Vector3(10, 0, 0));
 		terrainPosition.Add (new Vector3(-10, 0, 0));
@@ -31,30 +39,23 @@ public class EventGenerator2 : MonoBehaviour{
 		terrainPosition.Add (new Vector3(-10, 0, -10));
 		
 
-
+		GenerateEvent ();
 	}
-		
 
-	//generoi eventin, ottaa inputtina string tileType jonka perusteella luo alueen
+	public void testi() {
+		npc.GenerateNPC ("velho");
+		Debug.Log (npc.name);
+	}
+
+
 	public void GenerateEvent() {
 	
 		foreach (GameObject o in tempList)
 			Destroy (o);
 
-		//LUO VUORI EVENT
-
-
-
-
-		/*
-
-		// LUO METSA EVENT
-
 		//Generate tiles
-
-
 		for (int i = 0; i < terrainPosition.Count; i++) {
-			Tile tileTemp = new Tile ("tile_" + i, 10, 1, 10, terrainPosition [i], terrainMaterial [Random.Range (0, 2)]);
+			Tile tileTemp = new Tile ("tile_" + i, 10, 1, 10, terrainPosition [i], terrainMaterial [Random.Range (0, 3)]);
 			tileTemp.instantiateTile (tileTemp);
 		}
 		//Generate trees & rocks
@@ -64,23 +65,33 @@ public class EventGenerator2 : MonoBehaviour{
 			GameObject asset = objectList [Random.Range (0, 3)];
 			Vector3 position = new Vector3 (Random.Range (-assetRangeX, assetRangeX), 0, Random.Range (-assetRangeZ, assetRangeZ));
 
-			//Debug.DrawLine (position, new Vector3 (position.x, 10, position.z), Color.green, 60f);
+//			Debug.DrawLine (position, new Vector3 (position.x, 10, position.z), Color.green, 60f);
 
 			if (Physics.Raycast (new Vector3 (position.x, 10, position.z), -Vector3.up, 5f) == false) {
-				GameObject tempAsset = Instantiate (asset, position, Quaternion.identity);
-				tempList.Add (tempAsset);
+				Debug.Log ("Ei osunut");
+				GameObject temp = Instantiate (asset, position, Quaternion.identity);
+				if (temp.GetComponent<Rigidbody> ()) {
+					temp.GetComponent<Rigidbody> ().centerOfMass = new Vector3 (0f, -3f, 0f);
+				}
+				assetList.Add (temp);
 				j++;
 			} else {
 				j++;
 			}
 
-			}
-		*/
-			
-
 		}
-
-		
-
+		AddItems ();
 	}
+
+
+	void AddItems(){
+		int itemCount = 8;
+		for (int i = 0; i < itemCount; i++) {
+			Vector3 position = new Vector3 (Random.Range (-assetRangeX, assetRangeX), 1, Random.Range (-assetRangeZ, assetRangeZ));
+			GameObject temp = Instantiate (item, position, Quaternion.identity);
+			temp.GetComponent<Item> ().GenerateItem ("Ankka");
+		}
+	}
+
+}
 	
