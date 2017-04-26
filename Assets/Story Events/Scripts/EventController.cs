97e5d0240ed075d7ab272bfe04ee846b66061af1
PyryKa <kanervapyry@gmail.com>
2017-04-26 11:51:53 +0300

    Story Events
    Beginning framework done

---
+++ Assets/Story Events/Scripts/EventController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventController : MonoBehaviour {
+
+	public GameObject player;
+	public Camera eventCamera;
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+
+	public void TeleportPlayer(Vector3 position) {
+		player.transform.position = position;
+	}
+
+}
---
+++ Assets/Story Events/Scripts/TriggerScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour {
+
+	public EventController eventController;
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("osu");
+		eventController.TeleportPlayer (new Vector3(0,0.5f,0));
+	}
+}
