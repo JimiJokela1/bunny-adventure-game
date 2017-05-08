6e2ed130fca5543b5a2db412f9d3b28a856ccd7f
PyryKa <kanervapyry@gmail.com>
2017-05-08 08:15:13 +0300

    Finished

--- Assets/Event/Scripts/Tile.cs
+++ Assets/Event/Scripts/Tile.cs
@@ -8,9 +8,17 @@ public class Tile {
 	public int tileSizeX; //tile sizes
 	public int tileSizeZ;
 	public int tileSizeY;
-	public Material tileMaterial; //tile materia
+	public Material tileMaterial; //tile material
 
-	// CONSTRUCTOR
+	/// <summary>
+	/// Initializes a new instance of the Tile class.
+	/// </summary>
+	/// <param name="tileName">Tile name.</param>
+	/// <param name="tileSizeX">Tile size x.</param>
+	/// <param name="tileSizeZ">Tile size z.</param>
+	/// <param name="tileSizeY">Tile size y.</param>
+	/// <param name="tilePosition">Tile position.</param>
+	/// <param name="tileMaterial">Tile material.</param>
 	public Tile(string tileName, int tileSizeX, int tileSizeZ, int tileSizeY, Vector3 tilePosition, Material tileMaterial) {
 		this.tileName = tileName;
 		this.tilePosition = tilePosition;
@@ -20,7 +28,10 @@ public class Tile {
 		this.tileMaterial = tileMaterial;
 	}
 
-	// METHOD FOR INSTANTIATION
+	/// <summary>
+	/// Instantiates the tile to the gameworld.
+	/// </summary>
+	/// <param name="tile">Tile.</param>
 	public void instantiateTile(Tile tile) {
 		GameObject tileTemp = GameObject.CreatePrimitive (PrimitiveType.Cube);
 		tileTemp.name = tile.tileName;
--- Assets/Story Events/Scripts/EventController.cs
+++ Assets/Story Events/Scripts/EventController.cs
@@ -8,7 +8,8 @@ public class EventController : MonoBehaviour {
 	int sceneProgress = 0; //keeps track of scene progress
 	GameObject player;
 	public Camera eventCamera;
-	public GameObject[] teleportTargets;
+	public GameObject[] teleportTargets; //holds the targets where player is teleported from triggerplates
+
 	void Start() {
 		player = GameObject.Find ("EventPlayer");
 		teleportTargets = GameObject.FindGameObjectsWithTag ("teleport").OrderBy (go => go.name).ToArray ();
@@ -20,17 +21,18 @@ public class EventController : MonoBehaviour {
 
 	}
 
-
+	/// <summary>
+	/// Triggers the event, teleports the player and increases sceneprogress
+	/// </summary>
 	public void TriggerEvent() {
 		player.transform.position = teleportTargets[sceneProgress].transform.position;
 		Debug.Log (sceneProgress);
 		sceneProgress++;
 	}
-
-	public void TeleportPlayer(Vector3 position) {
-		player.transform.position = position;
-	}
-
+	/// <summary>
+	/// Completes the event.
+	/// </summary>
+	/// <param name="progressID">Progress I.</param>
 	public void CompleteEvent(int progressID){
 		PlayerController.Instance.AddToProgress (progressID);
 	}
